a9af58068eab0e2299af854bb1a544011872c64d
Rafael José da Costa Glória <rafaeljcg20@gmail.com>
2019-12-26 16:30:28 -0300

    [ADD]Criando configurações dos containers de serviços do sistema.

--- application/config/example-database.php
+++ application/config/example-database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'engajamento',
+	'hostname' => '@@DB_HOST@@',
+	'username' => '@@DB_USERNAME@@',
+	'password' => '@@DB_PASSWORD@@',
+	'database' => '@@DB_NAME@@',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
