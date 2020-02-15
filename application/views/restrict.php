cb2f2a1cb6072fa09d2789cd149dedf52c75e481
Douglasx <douglasantana007@gmail.com>
2020-02-15 21:35:13 -0200

    [DELETE]:hammer: Removendo arquitetura Codeigniter.

--- application/views/restrict.php
+++
@@ -1,9 +0,0 @@
-
-<!-- Begin page content -->
-<main role="main" class="flex-shrink-0">
-  <div class="container">
-    <h1 class="mt-5">Projeto Engajamento - Sara Nossa Terra</h1>
-    <p class="lead">Aqui estaremos iniciando o desenvolvimento do nosso sistema.</p>
-    <p>Vamos programar!</p>
-  </div>
-</main>
