e3ac5fd68aa298285723a9d65d1f04894e374648
Douglas <douglasantana007@gmail.com>
2020-05-24 00:03:15 -0300

    [WIP/ADD]:rocket: Adding AdminLTE.
    - Refactoring old layout to admin lte.

--- application/resources/views/revisao/index.blade.php
+++ application/resources/views/revisao/index.blade.php
@@ -1,41 +1,59 @@
-@extends('layouts.app')
+@extends('adminlte::page')
+
+@section('title', 'Revisão')
+
+@section('content_header')
+    <h1>Lista de Revisão</h1>
+@stop
 
 @section('content')
-    <div class="container">
 
-        <h4 class="text-center">Lista de Revisão</h4>
-        <a href="{{ route('revisao.create') }}" class="btn-novo btn btn-success">
-            <i class="fa fa-plus"></i>&nbsp;Novo
-        </a>
-        <div class="table-responsive">
-            <table class="table table-striped table-hover table-bordered dataTable">
-                <thead class="thead-light">
-                <tr>
-                    <th>Ações</th>
-                    <th scope="col">Data de Cadastro</th>
-                    <th scope="col">Data da Revisão</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($aItens as $pAjudadas)
-                    <tr>
-                        <td>
-                            <a href="{{ route('revisao.edit', base64_encode($pAjudadas->id)) }}"
-                               class="btn btn-primary" title="Editar">
-                                <span class="fa fa-edit"></span>
-                            </a>
-                            <a href="{{ route('revisao.delete', base64_encode($pAjudadas->id)) }}"
-                               class="btn btn-danger link-excluir" title="Excluir">
-                                <span class="fa fa-trash"></span>
-                            </a>
-                        </td>
-                        <td>{{ \Carbon\Carbon::parse($pAjudadas->dt_cadastro)->format('d/m/Y') }}</td>
-                        <td>{{ \Carbon\Carbon::parse($pAjudadas->dt_revisao)->format('d/m/Y') }}</td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-            {{ $aItens->links() }}
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Revisões</h3>
+                    <div class="card-tools">
+                        <a href="{{ route('revisao.create') }}" class="btn-novo btn btn-success float-right">
+                            <i class="fa fa-plus"></i>&nbsp;Novo
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                        <tr>
+                            <th style="width: 15%">Ações</th>
+                            <th>Data de Revisão</th>
+                            <th>Data de Cadastro</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($aItens as $index => $revisao)
+                            <tr>
+                                <td>
+                                    <a href="{{ route('pessoasajudadas.edit', base64_encode($revisao->id)) }}"
+                                       class="btn btn-primary" title="Editar">
+                                        <span class="fa fa-edit"></span>
+                                    </a>
+                                    <a href="{{ route('pessoasajudadas.delete', base64_encode($revisao->id)) }}"
+                                       class="btn btn-danger link-excluir" title="Excluir">
+                                        <span class="fa fa-trash"></span>
+                                    </a>
+                                </td>
+                                <td>{{ \Carbon\Carbon::parse($revisao->dt_cadastro)->format('d/m/Y') }}</td>
+                                <td>{{ \Carbon\Carbon::parse($revisao->dt_revisao)->format('d/m/Y') }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer clearfix">
+                    <ul class="pagination pagination-sm m-0 float-right">
+                        {{ $aItens->links() }}
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
