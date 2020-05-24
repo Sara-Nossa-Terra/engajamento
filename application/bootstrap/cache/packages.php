6838454b4b01497f0bbb2b3066162a4c276a4d81
Douglas <douglasantana007@gmail.com>
2020-05-23 23:01:38 -0300

    [WIP/ADD]:rocket: Adding AdminLTE.
    - Adding admin template to the engajamento project.

--- application/bootstrap/cache/packages.php
+++ application/bootstrap/cache/packages.php
@@ -6,6 +6,13 @@
       0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
     ),
   ),
+  'jeroennoten/laravel-adminlte' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'JeroenNoten\\LaravelAdminLte\\AdminLteServiceProvider',
+    ),
+  ),
   'laravel/tinker' => 
   array (
     'providers' => 
