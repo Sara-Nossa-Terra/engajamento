1776991f7d84ae8ad41cec25f86ba0724c8a6099
Paulo Henrique <paulotux.mendes@gmail.com>
2020-08-13 20:07:48 -0300

    [UPDATE] Ajuste no docker-compose.yml, para receber váriaveis de ambiente

--- application/docker/php/docker-entrypoint.sh
+++ application/docker/php/docker-entrypoint.sh
@@ -18,7 +18,7 @@ if  ! [ -e "/var/www/html/.env" ] ; then
      && php artisan key:generate
 
      # Regex to many "/"
-    sed -i "s@APP_URL@$APP_URL@g" /var/www/html/.env
+    sed -i "s@HOME_URI_VAL@$HOME_URI_VAL@g" /var/www/html/.env
 
     sed -i "s/@@DB_CONNECTION@@/$DB_CONNECTION/g" /var/www/html/.env
     sed -i "s/@@DB_HOST@@/$DB_HOST/g" /var/www/html/.env
@@ -26,8 +26,6 @@ if  ! [ -e "/var/www/html/.env" ] ; then
     sed -i "s/@@DB_DATABASE@@/$DB_DATABASE/g" /var/www/html/.env
     sed -i "s/@@DB_USERNAME@@/$DB_USERNAME/g" /var/www/html/.env
     sed -i "s/@@DB_PASSWORD@@/$DB_PASSWORD/g" /var/www/html/.env
-
-
 fi
 
 echo "[ ****************** ] Ending Endpoint of Application [ ****************** ]"
